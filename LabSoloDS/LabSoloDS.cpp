b0acbbebd40b64d77a0281857220f414a7f030ee
vhakHCMUS <22127201@student.hcmus.edu.vn>
2023-03-04 20:43:44 +0700

    Menu array update

--- LabSoloDS/LabSoloDS.cpp
+++ LabSoloDS/LabSoloDS.cpp
@@ -5,9 +5,80 @@
 using namespace sf;
 using namespace std;
 
+void setupRectangles(sf::RenderWindow& window, sf::Font& font, std::vector<sf::RectangleShape>& rects, std::vector<int>& values, float &rectWidth, float &rectHeight, float &rectSpacing, float &rectX, float &rectY, int &numValues, bool& inputArray)
+{
+    if (inputArray)
+    {
+        std::cout << "Enter the number of values: ";
+        std::cin >> numValues;
 
+        // Prompt the user to enter the values
+        std::cout << "Enter the values:\n";
+        for (int i = 0; i < numValues; i++) {
+            int value;
+            std::cin >> value;
+            values.push_back(value);
+        }
+        inputArray = false;
 
+        for (int i = 0; i < numValues; i++) {
+            // Create the rectangle shape
+            sf::RectangleShape rect(sf::Vector2f(rectWidth, rectHeight));
+            rect.setPosition(rectX, rectY);
 
+            // Set the rectangle color
+            rect.setFillColor(sf::Color::Red);
+
+            // Create the text for the value
+            sf::Text text(std::to_string(values[i]), font, 20);
+            text.setFillColor(sf::Color::Black);
+            text.setPosition(rectX + rectWidth / 2.0f - text.getLocalBounds().width / 2.0f,
+                rectY + rectHeight / 2.0f - text.getLocalBounds().height / 2.0f);
+
+            // Add the rectangle and text to the vectors
+            rects.push_back(rect);
+
+            // Increment the rectangle position for the next rectangle
+            rectX += rectWidth + rectSpacing;
+        }
+    }
+}
+
+void drawRectangles(sf::RenderWindow& window, sf::Font &font,std::vector<sf::RectangleShape>& rects, std::vector<int>& values, float &rectHeight, float &rectWidth, int numValues)
+{
+    for (int i = 0; i < numValues; i++) {
+        window.draw(rects[i]);
+
+        sf::Text text(std::to_string(values[i]), font, 20);
+
+        text.setFillColor(sf::Color::Black);
+
+        text.setPosition(rects[i].getPosition().x + rectWidth / 2.0f - text.getLocalBounds().width / 2.0f,
+            rects[i].getPosition().y + rectHeight / 2.0f - text.getLocalBounds().height / 2.0f);
+
+        window.draw(text);
+    }
+}
+void drawArrayMenu(sf::RenderWindow& window, sf::Font& font, sf::Event& event)
+{
+    Button addButton(900.f, 200.f, 100.f, 50.f, font, "Add",
+        sf::Color::Blue, sf::Color::Yellow, sf::Color::Magenta);
+
+    Button deleteButton(900.f, 300.f, 100.f, 50.f, font, "Delete",
+        sf::Color::Blue, sf::Color::Yellow, sf::Color::Magenta);
+
+    Button updateButton(900.f, 400.f, 100.f, 50.f, font, "Update",
+        sf::Color::Blue, sf::Color::Yellow, sf::Color::Magenta);
+
+    Button searchButton(900.f, 500.f, 100.f, 50.f, font, "Search",
+        sf::Color::Blue, sf::Color::Yellow, sf::Color::Magenta);
+
+    addButton.render(window);
+    deleteButton.render(window);
+    updateButton.render(window);
+    searchButton.render(window);
+
+}
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1600, 900), "SFML Button Example");
@@ -59,54 +130,9 @@ int main()
 
         if (Array_clicked)
         {
-
-            if (inputArray)
-            {
-                std::cout << "Enter the number of values: ";
-                std::cin >> numValues;
-
-                // Prompt the user to enter the values
-                std::cout << "Enter the values:\n";
-                for (int i = 0; i < numValues; i++) {
-                    int value;
-                    std::cin >> value;
-                    values.push_back(value);
-                }
-                inputArray = false;
-
-                for (int i = 0; i < numValues; i++) {
-                    // Create the rectangle shape
-                    sf::RectangleShape rect(sf::Vector2f(rectWidth, rectHeight));
-                    rect.setPosition(rectX, rectY);
-
-                    // Set the rectangle color
-                    rect.setFillColor(sf::Color::Red);
-
-                    // Create the text for the value
-                    sf::Text text(std::to_string(values[i]), font, 20);
-                    text.setFillColor(sf::Color::Black);
-                    text.setPosition(rectX + rectWidth / 2.0f - text.getLocalBounds().width / 2.0f,
-                        rectY + rectHeight / 2.0f - text.getLocalBounds().height / 2.0f);
-
-                    // Add the rectangle and text to the vectors
-                    rects.push_back(rect);
-
-                    // Increment the rectangle position for the next rectangle
-                    rectX += rectWidth + rectSpacing;
-                }
-            }
-            for (int i = 0; i < numValues; i++) {
-                window.draw(rects[i]);
-
-                sf::Text text(std::to_string(values[i]), font, 20);
-
-                text.setFillColor(sf::Color::Black);
-
-                text.setPosition(rects[i].getPosition().x + rectWidth / 2.0f - text.getLocalBounds().width / 2.0f,
-                    rects[i].getPosition().y + rectHeight / 2.0f - text.getLocalBounds().height / 2.0f);
-
-                window.draw(text);
-            }
+            setupRectangles(window, font, rects, values, rectWidth, rectHeight, rectSpacing, rectX, rectY, numValues, inputArray);
+            drawRectangles(window, font, rects, values, rectHeight, rectWidth, numValues);
+            drawArrayMenu(window, font, event);
         }
 
 
